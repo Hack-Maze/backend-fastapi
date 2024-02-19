498e51beeed5b7925efd534f165a27d0c83e8b7b
Marym Magdy <101295012+marymfouda@users.noreply.github.com>
2024-02-19 18:39:50 +0000

    Add PublicUser endpoint

--- src/backend/app/app/api/api_v1/api.py
+++ src/backend/app/app/api/api_v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import items, login, users, utils, rooms
+from app.api.api_v1.endpoints import items, login, users, utils, rooms , publicuser
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
@@ -8,3 +8,4 @@ api_router.include_router(users.router, prefix="/users", tags=["users"])
 # api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 # api_router.include_router(items.router, prefix="/items", tags=["items"])
 api_router.include_router(rooms.router, prefix="/rooms", tags=["rooms"])
+api_router.include_router(publicuser.router, prefix="/publicuser", tags=["publicuser"])
